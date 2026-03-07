a55bf0587206f531f0fa2a99f657bfc670f658b5
Lucent Lu <1215.lucent@gmail.com>
2026-03-06 19:31:33 -0500

    lab1

--- Lab1.py
+++ Lab1.py
@@ -1,9 +1,13 @@
 from math import log
 import numpy as np
+import matplotlib.pyplot as plt
 
 #Activation Function and Derivative
-def sigmoid(x): return 1 / (1 + np.e**(-1*x))
-def sigmoidPrime(x): return np.e**(-1*x) / ((1 + np.e**(-1*x))**2) 
+def sigmoid(x): 
+    return 1 / (1 + np.exp(-x))
+
+def sigmoidPrime(x): 
+    return np.exp(-x) / ((1 + np.exp(-x))**2)
 
 
 #Some useful comversion functions
@@ -30,8 +34,8 @@ def architecture(new_list):
     biases.append(None)
     network_length = len(new_list)
     for c in range(network_length-1):
-        weight_matrix = 2 * np.random.rand(new_list[c+1], new_list[c]) - 1
-        bias_matrix = 2 * np.random.rand(new_list[c+1],1) - 1
+        weight_matrix = (2 * np.random.rand(new_list[c+1], new_list[c]) - 1) / np.sqrt(new_list[c])
+        bias_matrix = (2 * np.random.rand(new_list[c+1],1) - 1) / np.sqrt(new_list[c])
         weights.append(weight_matrix)
         biases.append(bias_matrix)
     return weights, biases
@@ -43,7 +47,8 @@ def read_file(file_name):
         for line in f:
             image = line[0:len(line)-1].split(",")
             output = image.pop(0)
-            in_vec = ListtoVector(image)
+            #in_vec = ListtoVector(image)
+            in_vec = np.array(image, dtype=float).reshape(784, 1) / 255.0
             out_vec = list()
             for c in range(10):
                 if c == int(output):
@@ -55,16 +60,90 @@ def read_file(file_name):
             toReturn.append(toAppend)
     return toReturn
 
+def eval(dataset, weights, biases):
+    correct_predictions = 0
+    for inp, label in dataset:
+        a, _ = p_net(weights, biases, inp)
+        prediction = np.argmax(a[-1])
+        actual = np.argmax(label)
+        if prediction == actual:
+            correct_predictions += 1
+    # Return accuracy as %
+    return (correct_predictions / len(dataset)) * 100
+
+def MSE(v1, v2):
+    c=0
+    for i in range(len(v1)):
+        c+=(v1[i]-v2[i])**2
+    return c/2
+
 #TODO A feed forward of the network where A_vec is the activation function, weights is a list of all the weight matrices, biases is a list of all the bias vectors, and inp is the input, return the output as a vector
-def p_net(A_vec, weights, biases, inp):
-    return None
+def p_net(weights: list[float], biases: list[float], inp: np.ndarray):
+    a=[inp]
+    z=[]
+    for l in range(1,len(weights)):
+        z.append(np.dot(weights[l],a[l-1])+biases[l])
+        a.append(sigmoid(z[l-1]))
+        
+    return a, z
 
 #TODO This is where you back propogate by calculating the deltas and updating the weights and biases, try different learning rates and see what works
-def one_epoch(training, weights, biases):
-    return weights, biases
+def one_epoch(training, weights, biases, LR=0.5):
+    i=0
+    input=training[i][0]
+    output, z=p_net(weights, biases, input)
+    c=MSE(output[-1], training[i][1])
+    
+    d=[(output[-1]-training[i][1])*sigmoidPrime(z[1])]
+    d.insert(0, (np.dot(np.transpose(weights[2]), d[0]))*sigmoidPrime(z[0]))
+    
+    dcdw=[]
+    dcdb=[]
+    for l in range(2):
+        dcdw.append(d[l]*np.transpose(output[l]))
+        dcdb.append(d[l])
+        
+    new_weights=[None]
+    new_biases=[None]
+    for i in [1,2]:
+        new_weights.append(weights[i]-LR*dcdw[i-1])
+        new_biases.append(biases[i]-LR*dcdb[i-1])
+
+    return new_weights, new_biases
 
 #TODO Run your model over some number of epochs should be at least 10 and display a graph that shows train and test accuracy on each Epoch
+if __name__ == "__main__":
+    training = read_file("mnist_train.csv")
+    testing = read_file("mnist_test.csv")
+    weights, biases = architecture([784, 30, 10])
 
+    epochs=50
+    train_accs=[]
+    test_accs=[]
+    
+    for epoch in range(epochs):
+        weights, biases = one_epoch(training, weights, biases, 0.01)
+        
+        train_acc = eval(training, weights, biases)
+        test_acc = eval(testing, weights, biases)
+        
+        train_accs.append(train_acc)
+        test_accs.append(test_acc)
+        
+        print(f"Epoch {epoch+1}/{epochs} | Train Acc: {train_acc:.2f}% | Test Acc: {test_acc:.2f}%")
+        
+    plt.figure(figsize=(8, 5))
+    plt.plot(range(1, epochs + 1), train_accs, label="Training Accuracy", marker='o')
+    plt.plot(range(1, epochs + 1), test_accs, label="Testing Accuracy", marker='s')
+    
+    plt.title("Neural Network Accuracy over Epochs")
+    plt.xlabel("Epoch")
+    plt.ylabel("Accuracy (%)")
+    plt.xticks(range(1, epochs + 1))
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+    
 
 
 
