c7f8970f7c8e4b6eed0f5bb57125f29f7ae3e587
Emily Silkina <emily.silkin@gmail.com>
2026-03-08 14:28:49 -0400

    Progress

--- Lab1.py
+++ Lab1.py
@@ -1,12 +1,13 @@
 from math import log
 import numpy as np
+import matplotlib.pyplot as plt
 
 #Activation Function and Derivative
 def sigmoid(x): return 1 / (1 + np.e**(-1*x))
 def sigmoidPrime(x): return np.e**(-1*x) / ((1 + np.e**(-1*x))**2) 
 
 
-#Some useful comversion functions
+#Some useful conversion functions
 def ListtoVector(new_list):
     length = len(new_list)
     vec = np.arange(length)
@@ -57,13 +58,58 @@ def read_file(file_name):
 
 #TODO A feed forward of the network where A_vec is the activation function, weights is a list of all the weight matrices, biases is a list of all the bias vectors, and inp is the input, return the output as a vector
 def p_net(A_vec, weights, biases, inp):
-    return None
+    a=inp
+    activationLayers=[a]
+    i=0
+    while i<len(weights):
+        z=np.dot(weights[i],a)+biases[i]
+        a=A_vec(z)
+        activationLayers.append(a)
+        i+=1
+    return activationLayers
 
 #TODO This is where you back propogate by calculating the deltas and updating the weights and biases, try different learning rates and see what works
 def one_epoch(training, weights, biases):
-    return weights, biases
+    learningRate=0.1
+    mse=0
+    correct=0
+
+    for x,y in training:
+        activations=p_net(sigmoid, weights, biases, x)
+        predicted=activations[-1]
+        actual=x
+        delta=sigmoidPrime(activations[-1])*(predicted-actual)
+
+        s=predicted-actual
+        error = predicted - y
+        s += np.mean(error**2)
+        mse+=s
+
+        predictedNum = np.argMax(activations[-1])
+        expectedNum = np.argMax(training[-1])
+
+        if predictedNum==expectedNum:
+            correct+=1
+
+        i=len(weights)-1
+        while i>0:
+            newDelta=np.dot(weights[i].T, delta)*sigmoidPrime(activations[i-1])
+            weights[i]=weights[i]-learningRate*np.dot(delta,activations[i-1].T)
+            biases[i]=biases[i]-learningRate*delta
+            delta=newDelta
+            i-=1
+
+    mse = s/len(weights)
+    accuracy=correct/len(training)
+
+    return weights, biases, mse, accuracy
 
 #TODO Run your model over some number of epochs should be at least 10 and display a graph that shows train and test accuracy on each Epoch
+def model(numEpochs, testFile, trainFile):
+    testinData=read_file(testFile)
+    trainingData=read_file(trainFile)
+    # for i in range(0, numEpochs):
+
 
 
 
